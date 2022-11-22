82c90522a2146e9cde42363dd45866e276de56b9
Kyle Larsen <kylelarsen.business@gmail.com>
2022-11-22 15:45:23 -0700

    added date format file.

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const User = require('./user.js');
+const Thought = require('./thought.js');
+const Reaction = require('./reaction.js');
+
+module.exports = {
+    User,
+    Thought,
+    Reaction
+};
