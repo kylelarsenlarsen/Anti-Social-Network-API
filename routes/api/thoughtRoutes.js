2c67a51bf61b4802c4ade67bd0bfecf6f1403b67
Kyle Larsen <kylelarsen.business@gmail.com>
2022-11-23 16:50:29 -0700

    build user routes.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,15 +1,21 @@
 const router = require('express').Router();
 const {
-getSingleThought,
-getThoughts,
-createThought,
-deleteThought,
-updateThought,
-addReaction,
-updateReaction
+    getSingleThought,
+    getThoughts,
+    createThought,
+    deleteThought,
+    updateThought,
+    addReaction,
+    updateReaction
 } = require('');
 
 // need to create a router that gets thoughts and posts thoughts.
+router.route('/').get(getThoughts).post(createThought);
 
+// the following router will return one thought, json body, and GUID.
+router.route('/:_id').get(getSingleThought).delete(deleteThought).put(updateThought);
+router.route('/addReaction/:_id').put(addReaction);
+router.route('/:_thoughtId/reactions/:_reactionId').put(updateReaction);
 
-// the following router will return one thought, json body, and guid.
+module.exports = router;
+
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+    getSingleUser,
+    getUsers,
+    createUser,
+    deleteUser,
+    updateUser,
+    addFriend,
+    updateFriends
+} = require('');
+
+// the following routers will get a single user, all users, delete users, update friends, and add friends.
+router.route('/:_id').get(getSingleUser);
+router.route('/').get(getUsers).post(createUser);
+router.route('/:_id').delete(deleteUser).put(updateUser);
+router.route('/friends/:_id').put(addFriend);
+router.route('/removeFriend/:_id').put(updateFriends);
+
+module.exports = router;
+
