6344c2a76ecd894e8cc93e5586c2623cddc86cc7
Kyle Larsen <kylelarsen.business@gmail.com>
2022-11-23 16:07:55 -0700

    finishing up structure for thoughtroutes.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 const {
-
+getSingleThought,
+getThoughts,
+createThought,
+deleteThought,
+updateThought,
+addReaction,
+updateReaction
 } = require('');
 
 // need to create a router that gets thoughts and posts thoughts.
 
 
-// 
+// the following router will return one thought, json body, and guid.
